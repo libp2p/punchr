d41d6d1c0e3aa94141d344069151001288ffad97
Dennis Trautwein <git@dtrautwein.eu>
2022-11-08 13:50:24 +0100

    add arm compilation steps

--- crosscompile.sh
+++ crosscompile.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 oss=(darwin linux windows freebsd netbsd openbsd)
-archs=(amd64 arm64 386 arm)
+archs=(amd64 arm64 386)
 
 for os in "${oss[@]}"; do
   for arch in "${archs[@]}"; do
     echo "Compiling $os $arch..."
     GOOS=${os} GOARCH=${arch} go build -ldflags="-X 'pkg/client/client.Version=$(cat cmd/client/version)'" -o dist/punchr_cli_${os}_${arch} cmd/client/*.go
   done
+done
+
+arms=(5 6 7)
+for arm in "${arms[@]}"; do
+    echo "Compiling linux arm$arm..."
+    GOOS=linux GOARCH=arm GOARM=${arm} go build -ldflags="-X 'pkg/client/client.Version=$(cat cmd/client/version)'" -o dist/punchr_cli_linux_arm${arm} cmd/client/*.go
 done
